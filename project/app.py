1ee5a95a7e6ea50fb1f8f1db4baa0e3fa286c2de
yitongknows <61811966+yitongknows@users.noreply.github.com>
2020-10-29 23:28:38 -0400

    added login logout page in app.py

--- project/app.py
+++ project/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
 import sqlite3
-from flask import Flask, g
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for
 
 # configuration
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
 
 # create and initialize a new Flask app
 app = Flask(__name__)
@@ -49,6 +50,29 @@ def index():
     cur = db.execute('select * from entries order by id desc')
     entries = cur.fetchall()
     return render_template('index.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
