bca6f1bbcfb7a51796c38b0f007f9d6e8ce3ca60
yitongknows <61811966+yitongknows@users.noreply.github.com>
2020-10-29 23:31:11 -0400

    added the style.css to the project

--- project/app.py
+++ project/app.py
@@ -74,5 +74,19 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run()
